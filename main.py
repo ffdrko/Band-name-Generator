cdbd261c112a834cd308ff06bd306bfba1f27c7c
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2024-05-03 16:01:40 +0600

    Band Name Generator program

---
+++ main.py
@@ -0,0 +1,7 @@
+print(" ----- Band Name Generator -----")
+user_city = input("Enter your city name you grew up in:\n")
+user_petName = input("Enter your pet name:\n")
+
+brand_name = user_city + " " + user_petName
+
+print("Your band name could be " + brand_name)
